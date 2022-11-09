5a3f53e9c10072265fc088174baf36998ba5a65a
Rufat <mr.smashlincer@gmail.com>
2022-11-09 22:57:29 +0500

    инициализировал компонент добавления todo

---
+++ src/components/AddTodoItem.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let title = "Введите задачу которую хотите сделать!";
+    export let buttonTitle = "Add Todo";
+
+    let items = [];
+
+    function handleAddClick() {
+        items = [...items, "item"];
+    }
+
+    $: console.log(items);
+</script>
+
+<div class="main-container">
+    <label for="todo-text">{title}</label>
+    <input class="todo-input" id="todo-text" />
+    <button on:click={handleAddClick}>{buttonTitle}</button>
+</div>
+
+<style>
+    label {
+        color: cornsilk;
+    }
+    .main-container {
+        background-color: lightseagreen;
+        background-repeat: 5px;
+        padding: 10px;
+    }
+    .todo-input {
+        width: 100%;
+    }
+</style>
